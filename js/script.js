48fef0c558a39b6ea602209354aab5fdc9001d98
eva1606 <168004475+eva1606@users.noreply.github.com>
2024-07-02 21:18:47 +0300

    create recipe card

--- js/script.js
+++ js/script.js
@@ -15,7 +15,37 @@ document.addEventListener('DOMContentLoaded', function() {
         recipeContainer.innerHTML = '';
 
         recipes.forEach(recipe => {
-            
+            const recipeCard = document.createElement('div');
+            recipeCard.classList.add('recipe-card');
+    
+            const recipeImage = document.createElement('img');
+            recipeImage.src = recipe.image;
+            recipeImage.alt = recipe.name;
+            recipeCard.appendChild(recipeImage);
+    
+            const recipeInfo = document.createElement('div');
+            recipeInfo.classList.add('recipe-info');
+    
+            const recipeName = document.createElement('h3');
+            recipeName.textContent = recipe.name;
+            recipeInfo.appendChild(recipeName);
+    
+            const recipeDetails = document.createElement('div');
+            recipeDetails.classList.add('recipe-details');
+    
+            const recipeTime = document.createElement('span');
+            recipeTime.innerHTML = `<img src="images/time-icon.svg" alt="Time icon"> ${recipe.time}`;
+            recipeDetails.appendChild(recipeTime);
+    
+            const recipeDifficulty = document.createElement('span');
+            recipeDifficulty.innerHTML = `<img src="images/level-icon.svg" alt="Difficulty icon"> ${recipe.difficulty}`;
+            recipeDetails.appendChild(recipeDifficulty);
+    
+            const recipeConservation = document.createElement('span');
+            recipeConservation.innerHTML = `<img src="images/trash-icon.svg" alt="Conservation icon"> ${recipe.conservation}`;
+            recipeDetails.appendChild(recipeConservation);
+    
+            recipeInfo.appendChild(recipeDetails);
         });
     }
     loadRecipes();
